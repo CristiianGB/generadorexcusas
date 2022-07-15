ec7c1a559c28601c6ea0b353cb35592d54605102
CristiianGB <cristian.guardenyo@gmail.com>
2022-07-15 17:35:11 +0000

    My ExcuseGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("button").addEventListener("click", () => {
+    document.getElementById("excusa").innerHTML = getTheExcuse();
+  });
+  function getTheExcuse() {
+    let who = ["mi abuela", "mi iguana", "mi perro", "mi hijo"];
+    let what = ["chocado", "estampado", "roto", "comido"];
+    let when = [
+      "antes de venir",
+      "esta mañana",
+      "hace un momento",
+      "ahora mismo"
+    ];
+
+    let randomwho = Math.floor(Math.random() * who.length);
+    let randomwhat = Math.floor(Math.random() * what.length);
+    let randomwhen = Math.floor(Math.random() * when.length);
+
+    let excuse =
+      "¡No te lo vas a creer! " +
+      who[randomwho] +
+      " se ha " +
+      what[randomwhat] +
+      " " +
+      when[randomwhen];
+    return excuse;
+  }
 };
